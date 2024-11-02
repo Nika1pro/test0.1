3de3c8e7795074b0d1b26f30c07f2732aa2b418a
Nika1pro <veronika1prozorova@gmail.com>
2024-11-02 21:37:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>9.98, 1000!=1000.1)
+
+#3rd program
+task1=2*2+2
+print(task1)
+task2=2*(2+2)
+print(task2)
+print(task1==task2)
+
+#4th program
+number1=(float("123.456"))
+print(number1)
+number2=(number1)*10
+print(number2)
+number3=(number2)%4
+print(number3)
+print(number2!=number3)
+print(number1==number3)
+print(number1==10 and number2!=5)
